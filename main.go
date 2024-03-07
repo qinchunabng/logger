2177624a66604ee7d1acb4251b4d21e2708b6dde
qincb@leadingtek.com <qin15005234112>
2024-03-07 23:15:49 +0800

    20240307

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
-import (
-	"github.com/logger/logger"
-)
+import "github.com/logger/reflect"
 
-var log logger.Logger
+// var log logger.Logger
 
 func main() {
 	// log := logger.NewLog("info")
-	log = logger.NewFileLogger("info", "./", "logger.log", 1024*10)
-	id := 10010
-	name := "理想"
-	for {
-		log.Debug("这是一条debug日志")
-		log.Info("这是一条info日志,id:%d,name:%s", id, name)
-		log.Error("这是一条error日志")
-		// time.Sleep(time.Second * 3)
-	}
+	// log = logger.NewFileLogger("info", "./", "logger.log", 1024*10)
+	// id := 10010
+	// name := "理想"
+	// for {
+	// 	log.Debug("这是一条debug日志")
+	// 	log.Info("这是一条info日志,id:%d,name:%s", id, name)
+	// 	log.Error("这是一条error日志")
+	// 	// time.Sleep(time.Second * 3)
+	// }
+
+	var x int
+	reflect.ReflectType(x)
 }
